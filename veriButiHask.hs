329f339927f8bc36214e99fe1f0e3883cdff293b
AdriNicu <u1915748@campus.udg.edu>
2018-04-09 14:12:28 +0200

    Added new data types Ma and Basa.

--- veriButiHask.hs
+++ veriButiHask.hs
@@ -1,4 +1,5 @@
 --Implementació d'un validador de partides de Butifarra.
+
 --Definició dels nous tipus de dades
 --Trumfu: Pal de la Ma
 data Trumfu = Or | Ba | Co | Es | Bu deriving (Show, Eq) --Fer Trumfu mostrable i comparable
@@ -22,7 +23,7 @@ instance Eq Carta where --Fer Carta comparable
   (NewC (p1,t1)) == (NewC (p2,t2)) = (t1 == t2)
 
 instance Show Carta where --Fer Carta mostrable
-  show (NewC (p,t)) = ((show t) ++ "-" ++ (show p))
+  show (NewC (p,t)) =  "["++ ((show t) ++ " " ++ (show p)) ++ "]"
 
 instance Ord Carta where --Fer carta ordenable
   compare (NewC (p1,t1)) (NewC (p2,t2))
@@ -30,6 +31,23 @@ instance Ord Carta where --Fer carta ordenable
     | t1 <= t2 = LT
     | otherwise = GT
 
+--Ma: Representació de la ma d'un jugador.
+data Ma = NewM [Carta] deriving Eq --Fer Ma comparable
+
+instance Show Ma where --Fer Ma mostrable
+  show (NewM []) = "{}"
+  show (NewM (x:xs)) = ("{" ++ (show x) ++ "-" ++ (show2 (NewM xs)) ++ "}")
+--Funcio auxiliar per a fer el Show de la ma.
+show2 :: Ma->String
+show2 (NewM []) = ""
+show2 (NewM (x:[])) = (show x)
+show2 (NewM (x:xs)) = ((show x) ++ "-" ++ (show2 (NewM xs)))
+
+--Basa: Representació d'una ronda de tirades dels 4 jugadors
+data Basa = NewB (Integer, Carta, Carta, Carta, Carta) deriving Eq
+
+instance Show Basa where --Fer Basa mostrable.
+  show (NewB (j,w,x,y,z))= ("Tirada iniciada per el jugador: " ++ (show j) ++ "\n" ++ "  Tirada 1: " ++ (show w) ++ "\n" ++ "  Tirada 2: " ++ (show x) ++ "\n" ++ "  Tirada 3: " ++ (show y) ++ "\n" ++ "  Tirada 4: " ++ (show w))
 
 --Funcions Auxiliars
 --getCardValue: Retorna el valor numèric del Tipuscarta.
