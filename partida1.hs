e023e6ec909fc9a09e108a6f09ecb15db61bfa85
amatmv <amatmv@gmail.com>
2018-04-13 19:03:57 +0200

    Convert partida1 into readable Card array

--- partida1.hs
+++ partida1.hs
@@ -1,28 +1,10 @@
-% Podeu afegir les seg�ents constants al vostre codi, degudament transpormades les cartes als vostres tipus.
-% �s una partida correcte tenint en compte que comen�a el jugador 1 i que el trumfu �s Oros.
-% La puntuaci� per parelles �s 37 i 35.
+-- Podeu afegir les seg�ents constants al vostre codi, degudament transpormades les cartes als vostres tipus.
+-- �s una partida correcte tenint en compte que comen�a el jugador 1 i que el trumfu �s Oros.
+-- La puntuaci� per parelles �s 37 i 35.
 
 
-cartes1 =
- [
-  [NewC(Bastos, Manilla), NewC(Bastos, Vuit), NewC(Espases, Tres), NewC(Copes, As), NewC(Bastos, Quatre), NewC(Espases, Cavall), NewC(Copes, Set), NewC(Oros, As), NewC(Bastos, Cinc), NewC(Copes, Sota), NewC(Espases, Quatre), NewC(Bastos, Set)],
-  [NewC(Bastos, Sota), NewC(Bastos, Cavall), NewC(Espases, Manilla), NewC(Copes, Vuit), NewC(Oros, Cinc), NewC(Espases, Vuit), NewC(Copes, Manilla), NewC(Oros, Sis), NewC(Oros, Sota), NewC(Copes, Cinc), NewC(Oros, Set), NewC(Copes, Quatre)],
-  [NewC(Bastos, As), NewC(Oros, Dos), NewC(Espases, Rei), NewC(Copes, Cavall), NewC(Oros, Vuit), NewC(Espases, As), NewC(Copes, Rei), NewC(Oros, Rei), NewC(Copes, Tres), NewC(Copes, Sis), NewC(Espases, Sis), NewC(Espases, Cinc)],
-  [NewC(Bastos, Dos), NewC(Bastos, Tres), NewC(Espases, Dos), NewC(Copes, Dos), NewC(Bastos, Sis), NewC(Espases, Set), NewC(Oros, Tres), NewC(Oros, Quatre), NewC(Bastos, Rei), NewC(Oros, Cavall), NewC(Oros, Manilla), NewC(Espases, Sota)]
- ]
+cartes1 = [[NewC(Bastos, Manilla), NewC(Bastos, Vuit), NewC(Espases, Tres), NewC(Copes, As), NewC(Bastos, Quatre), NewC(Espases, Cavall), NewC(Copes, Set), NewC(Oros, As), NewC(Bastos, Cinc), NewC(Copes, Sota), NewC(Espases, Quatre), NewC(Bastos, Set)],[NewC(Bastos, Sota), NewC(Bastos, Cavall), NewC(Espases, Manilla), NewC(Copes, Vuit), NewC(Oros, Cinc), NewC(Espases, Vuit), NewC(Copes, Manilla), NewC(Oros, Sis), NewC(Oros, Sota), NewC(Copes, Cinc), NewC(Oros, Set), NewC(Copes, Quatre)],[NewC(Bastos, As), NewC(Oros, Dos), NewC(Espases, Rei), NewC(Copes, Cavall), NewC(Oros, Vuit), NewC(Espases, As), NewC(Copes, Rei), NewC(Oros, Rei), NewC(Copes, Tres), NewC(Copes, Sis), NewC(Espases, Sis), NewC(Espases, Cinc)],[NewC(Bastos, Dos), NewC(Bastos, Tres), NewC(Espases, Dos), NewC(Copes, Dos), NewC(Bastos, Sis), NewC(Espases, Set), NewC(Oros, Tres), NewC(Oros, Quatre), NewC(Bastos, Rei), NewC(Oros, Cavall), NewC(Oros, Manilla), NewC(Espases, Sota)]]
 
 
 
- partida1 = [
-	 Manilla Bastos, Sota Bastos, As Bastos, Dos Bastos,
-	 Vuit Bastos, Cavall Bastos, Dos Oros, Tres Bastos,
-	 Rei Espases, Dos Espases, Tres Espases, Manilla Espases,
-	 Vuit Copes, Cavall Copes, Dos Copes, As Copes,
-	 Quatre Bastos, Cinc Oros, Vuit Oros, Sis Bastos,
-	 As Espases, Set Espases, Cavall Espases, Vuit Espases,
-	 Rei Copes, Tres Oros, Set Copes, Manilla Copes,
-	 Quatre Oros, As Oros, Sis Oros, Rei Oros,
-	 Cinc Bastos, Sota Oros, Tres Copes, Rei Bastos,
-	 Cinc Copes, Sis Copes, Cavall Oros, Sota Copes,
-	 Manilla Oros, Quatre Espases, Set Oros, Sis Espases,
-	 Sota Espases, Set Bastos, Quatre Copes, Cinc Espases]
+ partida1 = [ NewC(Bastos, Manilla), NewC(Bastos, Sota), NewC(Bastos, As), NewC(Bastos, Dos), NewC(Bastos, Vuit), NewC(Bastos, Cavall), NewC(Oros, Dos), NewC(Bastos, Tres), NewC(Espases, Rei), NewC(Espases, Dos), NewC(Espases, Tres), NewC(Espases, Manilla), NewC(Copes, Vuit), NewC(Copes, Cavall), NewC(Copes, Dos), NewC(Copes, As), NewC(Bastos, Quatre), NewC(Oros, Cinc), NewC(Oros, Vuit), NewC(Bastos, Sis), NewC(Espases, As), NewC(Espases, Set), NewC(Espases, Cavall), NewC(Espases, Vuit), NewC(Copes, Rei), NewC(Oros, Tres), NewC(Copes, Set), NewC(Copes, Manilla), NewC(Oros, Quatre), NewC(Oros, As), NewC(Oros, Sis), NewC(Oros, Rei), NewC(Bastos, Cinc), NewC(Oros, Sota), NewC(Copes, Tres), NewC(Bastos, Rei), NewC(Copes, Cinc), NewC(Copes, Sis), NewC(Oros, Cavall), NewC(Copes, Sota), NewC(Oros, Manilla), NewC(Espases, Quatre), NewC(Oros, Set), NewC(Espases, Sis), NewC(Espases, Sota), NewC(Bastos, Set), NewC(Copes, Quatre), NewC(Espases, Cinc)]
