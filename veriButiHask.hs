0551b8f0121e1400966afe995efeb04fc17963c1
AdriNicu <u1915748@campus.udg.edu>
2018-04-18 19:31:56 +0200

    Improved the UI.

--- veriButiHask.hs
+++ veriButiHask.hs
@@ -14,17 +14,26 @@ import Data.STRef
 -- Definicions de tipus --
 --------------------------
 
-{-------- PAL: Pal de la basa ------
+
+{-------- PAL: Pal de les cartes ------
   És mostrable i comparable.
 -}
 data Pal = Oros | Bastos | Copes | Espases
            deriving (Show, Eq)
 
-{-------- TRUMFU: Pal de la Ma ------
+
+{-------- TRUMFU: Pal de la partia ------
   El Trumfu pot ser un Pal o "Butifarra".
   És mostrable i comparable.
 -}
 data Trumfu = Or | Ba | Co | Es | Bu
+              deriving (Show, Eq)
+
+{-------- MULTIPLICADOR: Multiplicadors de la partida ------
+  El Multiplicador es el valor per el qual es multiplica la puntuació.
+  És mostrable i comparable.
+-}
+data Multiplicador = Contro | Recontro | SantVicens | Barraca
               deriving (Show, Eq)
 
 {--------- TIPUSCARTA: Numero de la carta ------
@@ -39,6 +48,7 @@ instance Ord TipusCarta where
     | (getCardValue(tipuscarta1)) == (getCardValue(tipuscarta2)) = EQ
     | (getCardValue(tipuscarta1)) <= (getCardValue(tipuscarta2)) = LT
     | otherwise = GT
+
 
 {------ CARTA: Representació en Haskell d'una carta de la baralla espanyola. ------
   Una Carta Pertany a un Pal i és un tipus de carta (que representa el nombre de  l'1 al 12)
@@ -58,6 +68,7 @@ instance Ord Carta where
     | t1 <= t2 = LT
     | otherwise = GT
 
+
 {---- MA: Representació de la ma d'un jugador. ----
   S'entén per "Ma" el conjunt de Cartes que té el jugador a la seva mà.
   És comparable i mostrable.
@@ -67,12 +78,12 @@ data Ma = NewM [Carta] deriving Eq
 instance Show Ma where
   show (NewM []) = "{}"
   show (NewM (x:xs)) = ("{" ++ (show x) ++ "-" ++ (show2 (NewM xs)) ++ "}")
-
 -- Funció auxiliar per a fer el Show de la ma.
 show2 :: Ma -> String
 show2 (NewM []) = ""
 show2 (NewM (x:[])) = (show x)
 show2 (NewM (x:xs)) = ((show x) ++ "-" ++ (show2 (NewM xs)))
+
 
 {--- BASA: Representació d'una ronda de tirades dels 4 jugadors ----
   La Basa sempre conté quatre cartes i és iniciada per un jugador.
@@ -83,19 +94,44 @@ data Basa = NewB (Integer, Carta, Carta, Carta, Carta) deriving Eq
 instance Show Basa where --Fer Basa mostrable.
   show (NewB (j,w,x,y,z))= ("Tirada iniciada per el jugador: " ++ (show j) ++ "\n" ++ "  Tirada 1: " ++ (show w) ++ "\n" ++ "  Tirada 2: " ++ (show x) ++ "\n" ++ "  Tirada 3: " ++ (show y) ++ "\n" ++ "  Tirada 4: " ++ (show z))
 
+
 -------------------------------
 --- Definicio de la baralla ---
 -------------------------------
 
 baralla = [NewC(Oros, As), NewC(Oros, Dos), NewC(Oros, Tres), NewC(Oros, Quatre), NewC(Oros, Cinc), NewC(Oros, Sis), NewC(Oros, Set), NewC(Oros, Vuit), NewC(Oros, Manilla), NewC(Oros, Sota), NewC(Oros, Cavall), NewC(Oros, Rei), NewC(Espases, As), NewC(Espases, Dos), NewC(Espases, Tres), NewC(Espases, Quatre), NewC(Espases, Cinc), NewC(Espases, Sis), NewC(Espases, Set), NewC(Espases, Vuit), NewC(Espases, Manilla), NewC(Espases, Sota), NewC(Espases, Cavall), NewC(Espases, Rei), NewC(Bastos, As), NewC(Bastos, Dos), NewC(Bastos, Tres), NewC(Bastos, Quatre), NewC(Bastos, Cinc), NewC(Bastos, Sis), NewC(Bastos, Set), NewC(Bastos, Vuit), NewC(Bastos, Manilla), NewC(Bastos, Sota), NewC(Bastos, Cavall), NewC(Bastos, Rei), NewC(Copes, As), NewC(Copes, Dos), NewC(Copes, Tres), NewC(Copes, Quatre), NewC(Copes, Cinc), NewC(Copes, Sis), NewC(Copes, Set), NewC(Copes, Vuit), NewC(Copes, Manilla), NewC(Copes, Sota), NewC(Copes, Cavall), NewC(Copes, Rei)]
 
-----------------------
---- Loop principal ---
-----------------------
+
+-----------------------------------
+--- Implementació de la partida ---
+-----------------------------------
+
 jugar = do
-  putStrLn "--------------------"
-  putStrLn "--- VeriButiHask ---"
-  putStrLn "--------------------"
+  putStrLn "---------------------------------------------------------"
+  putStrLn "--------------------- VeriButiHask ----------------------"
+  putStrLn "---------------------------------------------------------"
+  putStrLn "                              _____"
+  putStrLn "  _____                _____ |6    |"
+  putStrLn " |2    | _____        |5    || & & |"
+  putStrLn " |  &  ||3    | _____ | & & || & & | _____"
+  putStrLn " |     || & & ||4    ||  &  || & & ||7    |"
+  putStrLn " |  &  ||     || & & || & & ||____9|| & & | _____"
+  putStrLn " |____Z||  &  ||     ||____S|       |& & &||8    | _____"
+  putStrLn "        |____E|| & & |              | & & ||& & &||9    |"
+  putStrLn "               |____h|              |____L|| & & ||& & &|"
+  putStrLn "                           _____           |& & &||& & &|"
+  putStrLn "                   _____  |K  WW|          |____8||& & &|"
+  putStrLn "           _____  |Q  ww| | o {)|                 |____6|"
+  putStrLn "    _____ |J  ww| | o {(| |o o%%| _____"
+  putStrLn "   |10 & || o {)| |o o%%| | |%%%||A _  |"
+  putStrLn "   |& & &||o o% | | |%%%| |_%%%>|| ( ) |"
+  putStrLn "   |& & &|| | % | |_%%%O|        |(_'_)|"
+  putStrLn "   |& & &||__%%[|                |  |  |"
+  putStrLn "   |___0I|                       |____V|"
+  putStrLn " "
+  putStrLn "---------------------------------------------------------"
+  putStrLn "---------------------------------------------------------"
+  putStrLn " "
   putStrLn "Entra una llavor per a generar la baralla: "
   llavor <- getLine
   putStrLn "S'ha generat la baralla. "
@@ -117,25 +153,95 @@ jugar = do
   -- putStrLn "Ma 4: "
   -- print(ma4)
   putStrLn "Cartes repartides als jugadors!"
+  putStrLn " "
   putStrLn "La teva ma és la següent: "
   putStrLn " "
   print(ma1)
   putStrLn " "
-  putStrLn "------   COMENÇA LA PARTIDA   ------"
-  mainLoop
-  putStrLn "End Exec"
+  putStrLn "El primer jugador a tirar ets tu!"
+  putStrLn " "
+  putStrLn "Escull un dels seguents Trumfus:"
+  escollirTrumfu ma1 ma2 ma3 ma4 1
 
-mainLoop = do
-  print("Blyat?")
+escollirTrumfu ma1 ma2 ma3 ma4 escollidor = do
+  putStrLn "->> Oros, Bastos, Copes, Espases, Butifarra <<-"
+  if escollidor == 1 then do --si escollim nosaltres
+    trumfu <- getLine
+    putStrLn " "
+    if trumfu == "Oros" then do
+      putStrLn "En aquesta partida el Trumfu escollit es Oros!"
+      contrarTime Or ma1 ma2 ma3 ma4 escollidor
+    else if trumfu == "Bastos" then do
+      putStrLn "En aquesta partida el Trumfu escollit es Bastos!"
+      contrarTime Ba ma1 ma2 ma3 ma4 escollidor
+    else if trumfu == "Copes" then do
+      putStrLn "En aquesta partida el Trumfu escollit es Copes!"
+      contrarTime Co ma1 ma2 ma3 ma4 escollidor
+    else if trumfu == "Espases" then do
+      putStrLn "En aquesta partida el Trumfu escollit es Espases!"
+      contrarTime Es ma1 ma2 ma3 ma4 escollidor
+    else if trumfu == "Butifarra" then do
+      putStrLn "En aquesta partida el Trumfu escollit es Butifarra!!!"
+      contrarTime Bu ma1 ma2 ma3 ma4 escollidor
+    else do
+      putStrLn "No es una opcio correcte, torna a escollir un  dels seguents Trumfus:"
+      escollirTrumfu ma1 ma2 ma3 ma4 escollidor
+  else do -- la maquina tria
+    putStrLn "WIP"
+    putStrLn "En aquesta partida el Trumfu escollit es Oros!"
+    contrarTime Or ma1 ma2 ma3 ma4 escollidor
+
+contrarTime trumfu ma1 ma2 ma3 ma4 escollidor = do
+  if (escollidor == 1) || (escollidor == 3) then do
+    if esContra2 trumfu ma2 ma4 then do --la maquina contra
+      if esRecontra1 trumfu ma3 then do --evalua si la teva parella recontraria
+        putStrLn "#La teva parella s'ha rascat la orella esquerra molt sutilment... estara volguent dir alguna cosa..."
+      else do
+        putStrLn " "
+      putStrLn "Jugador 1, vols recontrar?"
+      putStrLn "(S/N)"
+      eleccio <- getLine
+      if eleccio == "S" || eleccio == "s" then do
+        putStrLn "La parella formada per el jugador 1 i 3 ho tenen molt clar i recontren!"
+        putStrLn "WIP1"
+      else if eleccio == "N" || eleccio == "n" then do
+        putStrLn "La parella formada per el jugador 1 i 3  no ho han vist clar i han deixat passar la oportunitat de recontrar..."
+        putStrLn "Procedim doncs a comensar la partida sense multiplicadors de puntuacio."
+        mainLoop trumfu ma1 ma2 ma3 ma4 escollidor 2
+      else do
+        putStrLn "No es una opcio valida, torna a respondre:"
+        contrarTime trumfu ma1 ma2 ma3 ma4 escollidor
+    else do --la maquina no recontra
+      putStrLn "La parella formada per el jugador 2 i 4 no ho han vist clar i han deixat passar la oportunitat de contrar..."
+      putStrLn "Procedim doncs a comensar la partida sense multiplicadors de puntuacio."
+      mainLoop trumfu ma1 ma2 ma3 ma4 escollidor 1
+  else do
+    putStrLn "WIP2"
+
+mainLoop trumfu ma1 ma2 ma3 ma4 oldEscollidor multiplicador = do
+  putStrLn " "
+  putStrLn "------   COMENÇA LA PARTIDA   ------"
+  putStrLn " "
+  let newEscollidor = mod (oldEscollidor + 1) 4
   ent <- getLine
-  let input = (read ent)
-  if input == "Blyat"
-    then do putStrLn "GG WP"
-            mainLoop
-    else do putStrLn "Surrender"
+  if ent == "GG" then do
+    putStrLn "GG WP"
+    mainLoop trumfu ma1 ma2 ma3 ma4 oldEscollidor multiplicador
+  else do
+    putStrLn "Surrender"
+
+
 --------------------------
 --- Funcions Auxiliars ---
 --------------------------
+
+-- Aixo ha de dir si la IA hi aniria a l'hora de Contro Recontro SantVicens i Barraca polete
+esRecontra1 :: Trumfu -> Ma -> Bool --ToDo: Aqui necessitem la teva maestria polete!
+esRecontra1 trumfu maX = True
+esContra2 :: Trumfu -> Ma -> Ma -> Bool --ToDo: Aqui necessitem la teva maestria polete!
+esContra2 trumfu maX maY = True
+--
+
 
 crearMans :: [[Carta]] -> [Ma]
 crearMans llista = [(NewM (llista !! 0)),(NewM (llista !! 1)),(NewM (llista !! 2)),(NewM (llista !! 3))]
@@ -381,7 +487,7 @@ filtrarGuanyadoresFallantMirantSiTenimTrunfosSinoRetornaTotes l c t
   | ([x | x<-l, (trumfu2Pal t == (getPal x))] == []) = l
   | otherwise = [x | x<-l, (trumfu2Pal t == (getPal x))]
 
--- Donat un enter (mida llista 2 o 3), una llista de cartes i el trunfo retorna el jugador segons ordre de tirada que esta guanyant
+-- Donat un enter (mida llista 2 o 3), una llista de cartes i el trumfu retorna el jugador segons ordre de tirada que esta guanyant
 quiEstaGuanyant :: Integer -> [Carta] -> Trumfu -> Integer
 quiEstaGuanyant m (x:xs) t
   | (m == 2) && (getPal x == getPal (head xs)) = if x > head xs then 1 else 2
@@ -392,28 +498,28 @@ quiEstaGuanyant m (x:xs) t
   | (m == 3) && (quiEstaGuanyant 2 (take 2 (x:xs)) t == 2) = if quiEstaGuanyant 2 xs t == 1 then 2 else 3
 
 
--- Ma: ma del jugador  -- Trunfu: pal dominant de la partida -- [Carta] cartes de la base pot ser buida -- Return [Carta] possibles cartes
+-- Ma: ma del jugador  -- Trumfu: pal dominant de la partida -- [Carta] cartes de la base pot ser buida -- Return [Carta] possibles cartes
 -- Si cartes basa buida podem tirar qualsevol
 -- Si cartes basa size 1 (obligats a matar si podem):
         -- Si tenim del mateix pal i podem matar qualsevol que pugui matar
-        -- si fallem tirem qualsevol trunfo si tenim
-        -- si tampoc tenim trunfo tirem qualsevol
+        -- si fallem tirem qualsevol trumfu si tenim
+        -- si tampoc tenim trumfu tirem qualsevol
 -- Si cartes basa size 2 hem de mirar si el company [0] esta guanyant o no:
         -- Si company esta guanyant podem tirar qualsevol carta del mateix pal que a tirat i si fallem qualsevol altre
         -- altrament
-            -- si [1] no ha fallat hem de matar si podem amb cartes del mateix pal sino hem de tirar trunfo si tenim sino qualsevol
-            -- si [1] ha fallat (amb trunfo) i tenim encara cartes del pal iniciador podem tirar qualsevol del pal
-            -- Si [1] ha fallat (amb trunfo) i fallem tambe hem de matar amb trunfu si podem altrament qualsevol
+            -- si [1] no ha fallat hem de matar si podem amb cartes del mateix pal sino hem de tirar trumfu si tenim sino qualsevol
+            -- si [1] ha fallat (amb trumfu) i tenim encara cartes del pal iniciador podem tirar qualsevol del pal
+            -- Si [1] ha fallat (amb trumfu) i fallem tambe hem de matar amb trumfu si podem altrament qualsevol
 
 -- Si cartes base size 3 (el nostre company es [1]):
         -- si el company [1] esta guanyant podem tirar qualsevol carta del mateix pal que a tirat i si fallem qualsevol altre
         -- Altrament:
         -- mirem quina carta esta guanyant(Hem de matar si podem):
                 -- Si esta guanyant una carta del mateix pal que ha tirat el company:
-                    -- si tenim d'aquell pal i podem matar nomes podem tirar aquestes altrament si fallem trunfu si tenim altrament qualsevol
-                    -- si esta guanyant un fallo (de trunfo):
+                    -- si tenim d'aquell pal i podem matar nomes podem tirar aquestes altrament si fallem trumfu si tenim altrament qualsevol
+                    -- si esta guanyant un fallo (de trumfu):
                     -- si tenim del pal de la base qualsevol del pal
-                    -- Si fallem i tenim un trunfo que pugui matar qualsevol trunfo que pugui matar
+                    -- Si fallem i tenim un trumfu que pugui matar qualsevol trumfu que pugui matar
                     -- altrament qualsevol carta
 realJugadesPossibles :: Ma -> Trumfu -> [Carta] -> [Carta]
 realJugadesPossibles (NewM llista) _ [] = [x | x<-llista] -- Si no s'ha tirat cap carta començem nosaltres i podem tirar qualsevol carta
@@ -425,7 +531,7 @@ realJugadesPossibles (NewM llista) t (x:xs)
   | (length xs + 1 == 3) && (quiEstaGuanyant 3 (x:xs) t == 3) = if cartesPalMa (NewM llista) (getPal x) == [] then filtrarGuanyadoresFallantMirantSiTenimTrunfosSinoRetornaTotes [c | c<-llista] (last xs) t else filtrarGuanyadorasNoFallantSiNoPodemTotesLesDelPal (cartesPalMa (NewM llista) (getPal x)) (last xs)
   | otherwise = if cartesPalMa (NewM llista) (getPal x) == [] then [c | c<-llista] else cartesPalMa (NewM llista) (getPal x)
 
--- Donada les mans dels jugadors, el trunfu de la partida, les cartes jugades a la partida i el jugador que ha començat 
+-- Donada les mans dels jugadors, el trumfu de la partida, les cartes jugades a la partida i el jugador que ha començat
 -- retorna una tupla amb la base, numero de base i jugador que ha fet trampa
 
 --baseEsTrampa ::
